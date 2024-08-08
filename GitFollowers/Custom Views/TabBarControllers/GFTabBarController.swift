cab62630203ba8f464a347d335a43d94b993985f
ihebmbarki <iheb.mbarki@esprit.tn>
2024-08-08 17:25:19 +0100

    Code optimization

---
+++ GitFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  GFTabBarController.swift
+//  GitFollowers
+//
+//  Created by Iheb Mbarki on 1/8/2024.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAlertContainerView.swift
+//  GitFollowers
+//
+//  Created by Iheb Mbarki on 1/8/2024.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
